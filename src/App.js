0ca41368c47ab556514c77f608d9b4e72d398ee9
GarifullinII <ildaridrisovich@gmail.com>
2021-07-15 14:51:59 +0400

    created new component Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './img/logo.svg';
-import cart from './img/cart.svg';
-import heart from './img/heart.svg';
-import user from './img/user.svg';
-import btnPlus from './img/btn-plus.svg';
 import sneakers2 from './img/sneakers/sneakers2.jpg';
-import sneakers3 from './img/sneakers/sneakers3.jpg';
 import sneakers4 from './img/sneakers/sneakers4.jpg';
 import search from './img/search.svg';
 import remove from './img/btn-remove.svg';
 import arrow from './img/arrow.svg';
 import Card from './components/card/Card';
+import Header from "./components/header/Header";
 
 const App = () => {
   return (
@@ -74,27 +69,8 @@ const App = () => {
             </div>
         </div>
 
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-            <img width={40} height={40} src={logo} alt="logo" />
-            <div>
-                <h3 className="text-uppercase">The Best Sneakers</h3>
-                <p className="opacity-5">shop of the best sneakers</p>
-            </div>
-        </div>
-        <ul className="d-flex">
-            <li className="mr-30">
-                <img width={18} height={18} src={cart} alt="cart" />
-                <span>230 $</span>
-            </li>
-            <li className="mr-30">
-                <img width={18} height={18} src={heart} alt="like" />
-            </li>
-            <li>
-                <img width={18} height={18} src={user} alt="user" />
-            </li>
-        </ul>
-      </header>
+        <Header/>
+
         <div className="content p-40">
 
             <div className="d-flex align-center justify-between mb-40">
@@ -106,49 +82,8 @@ const App = () => {
             </div>
 
             <div className="d-flex">
-                <Card/>
 
-                <div className="card">
-                    <img width={133} height={112} src={sneakers2} alt="sneakers"/>
-                    <h5>Nike Air Max 270 Mens Trainers</h5>
-                    <div className="d-flex justify-between align-center">
-                        <div className="d-flex flex-column">
-                            <span>Price</span>
-                            <b>300 $</b>
-                        </div>
-                        <button className="button">
-                            <img width={11} height={11} src={btnPlus} alt="plus" />
-                        </button>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img width={133} height={112} src={sneakers3} alt="sneakers"/>
-                    <h5>Nike Blazer Mid Suede Mens Trainers</h5>
-                    <div className="d-flex justify-between align-center">
-                        <div className="d-flex flex-column">
-                            <span>Price</span>
-                            <b>300 $</b>
-                        </div>
-                        <button className="button">
-                            <img width={11} height={11} src={btnPlus} alt="plus" />
-                        </button>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <img width={133} height={112} src={sneakers4} alt="sneakers"/>
-                    <h5>Puma X Aka Boku Future Rider Mens Trainers</h5>
-                    <div className="d-flex justify-between align-center">
-                        <div className="d-flex flex-column">
-                            <span>Price</span>
-                            <b>350 $</b>
-                        </div>
-                        <button className="button">
-                            <img width={11} height={11} src={btnPlus} alt="plus" />
-                        </button>
-                    </div>
-                </div>
+                <Card />
 
             </div>
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from "../../img/logo.svg";
+import cart from "../../img/cart.svg";
+import heart from "../../img/heart.svg";
+import user from "../../img/user.svg";
+
+const Header = () => {
+    return (
+        <header className="d-flex justify-between align-center p-40">
+            <div className="d-flex align-center">
+                <img width={40} height={40} src={logo} alt="logo" />
+                <div>
+                    <h3 className="text-uppercase">The Best Sneakers</h3>
+                    <p className="opacity-5">shop of the best sneakers</p>
+                </div>
+            </div>
+            <ul className="d-flex">
+                <li className="mr-30">
+                    <img width={18} height={18} src={cart} alt="cart" />
+                    <span>230 $</span>
+                </li>
+                <li className="mr-30">
+                    <img width={18} height={18} src={heart} alt="like" />
+                </li>
+                <li>
+                    <img width={18} height={18} src={user} alt="user" />
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
