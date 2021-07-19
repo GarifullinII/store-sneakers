5733d1fce59c8b724b55cdf4dc0c5503a82ac47e
GarifullinII <ildaridrisovich@gmail.com>
2021-07-19 20:33:51 +0400

    refactor code for Drawer

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -17,73 +17,76 @@ const Drawer = ({ onClose,onRemove, items = [] }) => {
                         alt="remove"/>
                 </h2>
 
-                {
-                    items.length > 0 ? (
+                {items.length > 0 ? (
+
+                    <div>
+
                         <div className="items">
 
-                        {
-                            items.map((obj) => (
-                                <div>
+                            {
+                                items.map((obj) => (
+                                    <div>
 
-                                    <div className="cartItem d-flex align-center mb-20">
-                                        <div
-                                            style={{backgroundImage:`url(${obj.imgUrl})`}}
-                                            className="cartItemImg"
-                                        >
+                                        <div className="cartItem d-flex align-center mb-20">
+                                            <div
+                                                style={{backgroundImage:`url(${obj.imgUrl})`}}
+                                                className="cartItemImg"
+                                            >
+                                            </div>
+                                            <div className="mr-20 flex">
+                                                <p className="mb-5">
+                                                    {obj.title}
+                                                </p>
+                                                <b>
+                                                    {obj.price}
+                                                </b>
+                                            </div>
+                                            <img
+                                                onClick={() => onRemove(obj.id)}
+                                                className="removeBtn"
+                                                src={remove}
+                                                alt="remove"
+                                            />
                                         </div>
-                                        <div className="mr-20 flex">
-                                            <p className="mb-5">
-                                                {obj.title}
-                                            </p>
-                                            <b>
-                                                {obj.price}
-                                            </b>
-                                        </div>
-                                        <img
-                                            onClick={() => onRemove(obj.id)}
-                                            className="removeBtn"
-                                            src={remove}
-                                            alt="remove"
-                                        />
                                     </div>
-
-                                    <div className="cartTotalBlock">
-
-                                        <ul>
-                                            <li>
-                                                <span>Total:</span>
-                                                <div></div>
-                                                <b>650 $</b>
-                                            </li>
-                                            <li>
-                                                <span>Tax 5%:</span>
-                                                <div></div>
-                                                <b>32 $</b>
-                                            </li>
-                                        </ul>
-
-                                        <button className="greenButton">Checkout <img src={arrow} alt="arrow"/></button>
-
-                                    </div>
-
-                                </div>
-                            ))
-                        }
+                                ))}
                         </div>
+
+                        <div className="cartTotalBlock">
+
+                            <ul>
+                                <li>
+                                    <span>Total:</span>
+                                    <div></div>
+                                    <b>650 $</b>
+                                </li>
+                                <li>
+                                    <span>Tax 5%:</span>
+                                    <div></div>
+                                    <b>32 $</b>
+                                </li>
+                            </ul>
+
+                            <button className="greenButton">Checkout <img src={arrow} alt="arrow"/></button>
+
+                        </div>
+
+                    </div>
+
                     ) : (
-                        <div className="cartEmpty d-flex align-center justify-center flex-column flex">
-                            <img className="mb-20" width="120px" height="120px" src={emptyCart} alt="basket is empty"/>
-                            <h2>Basket is empty</h2>
-                            <p className="opacity-6">
-                                Add at least one pair of sneakers to order
-                            </p>
-                            <button className="greenButton">
-                                <img src={arrow} alt="arrow"/>
-                                Come back
-                            </button>
-                        </div>
-                    )
-                }
+                    <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+                        <img className="mb-20" width="120px" height="120px" src={emptyCart} alt="basket is empty"/>
+                        <h2>Basket is empty</h2>
+                        <p className="opacity-6">
+                            Add at least one pair of sneakers to order
+                        </p>
+                        <button onClick={onClose} className="greenButton">
+                            <img src={arrow} alt="arrow"/>
+                            Come back
+                        </button>
+                    </div>
+                    )}
+
 
             </div>
         </div>
