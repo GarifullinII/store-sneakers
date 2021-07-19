582128412ef72a0cd4a3a45c90fa840d525cde24
GarifullinII <ildaridrisovich@gmail.com>
2021-07-19 12:37:26 +0400

    added axios

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import search from './img/search.svg';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+import remove from "./img/btn-remove.svg";
 
 const App = () => {
 
     const [items, setItems] = React.useState([]);
 
     const [cartItems, setCartItems] = React.useState([]);
+
+    const [drawerItems, setDrawerItems] = React.useState([]);
+
+    const [searchValue, setSearchValue] = React.useState('');
 
     const [cartOpened, setCartOpened] = React.useState(false);
 
@@ -21,7 +26,11 @@ const App = () => {
     const onAddToCart = (obj) => {
         setCartItems(prev => [...prev, obj]);
 
-    }
+    };
+
+    const onChangeSearchInput = (event) => {
+        setSearchValue(event.target.value);
+    };
 
   return (
     <div className="wrapper clear">
@@ -29,29 +38,45 @@ const App = () => {
             onClickCart={() => {setCartOpened(true)}}
         />
 
-        {cartOpened && <Drawer items={cartItems} onClose={() => {setCartOpened(false)}}/>}
+        {cartOpened &&
+            <Drawer
+                items={cartItems}
+                onClose={() => {setCartOpened(false)}}
+            />
+        }
 
         <div className="content  p-40">
 
             <div className="d-flex align-center justify-between mb-40">
-                <h1>All sneakers</h1>
+                <h1>{searchValue ? `Search by request: "${searchValue}"` : 'All sneakers'}</h1>
                 <div className="search-block d-flex">
                     <img src={search} alt="search"/>
-                    <input placeholder="search ..."/>
+                    {searchValue && (
+                        <img
+                            onClick={() => setSearchValue('')}
+                            className="clear cu-p"
+                            src={remove}
+                            alt="clear"
+                        />
+                    )}
+                    <input onChange={onChangeSearchInput} value={searchValue} placeholder="search ..."/>
                 </div>
             </div>
 
             <div className="d-flex flex-wrap">
 
                 {
-                    items.map((item) => (
-                        <Card
-                            title={item.title}
-                            price={item.price}
-                            imgUrl={item.imgUrl}
-                            onFavorite={() => console.log("bookmarked")}
-                            onPlus={(obj) => onAddToCart(obj)}
-                        />
+                    items
+                        .filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((item, index) => (
+                            <Card
+                                key={index}
+                                title={item.title}
+                                price={item.price}
+                                imgUrl={item.imgUrl}
+                                onFavorite={() => console.log("bookmarked")}
+                                onPlus={(obj) => onAddToCart(obj)}
+                            />
                     ))
                 }
 
